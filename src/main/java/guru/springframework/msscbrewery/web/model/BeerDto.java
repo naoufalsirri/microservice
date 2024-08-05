d662240b245626d05dba17ccb3db03a45a334b1f
Naoufal <naoufal.sr10@gmail.com>
2024-08-05 20:55:09 +0200

    first work spring cloud

---
+++ src/main/java/guru/springframework/msscbrewery/web/model/BeerDto.java
@@ -0,0 +1,20 @@
+package guru.springframework.msscbrewery.web.model;
+
+import lombok.*;
+
+import java.util.UUID;
+
+/**
+ * Created by jt on 2019-04-20.
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class BeerDto {
+
+    private UUID id;
+    private String beerName;
+    private String beerStyle;
+    private Long upc;
+}
